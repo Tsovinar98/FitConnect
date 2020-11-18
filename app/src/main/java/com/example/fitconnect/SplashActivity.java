ade96ab7c8dc91640dadf4f3d9ddcfdc460480cf
Sovi <tgaspary@uncc.edu>
2020-11-18 15:36:13 -0500

    changed pic file names, added fade in animation

--- app/src/main/java/com/example/fitconnect/SplashActivity.java
+++ app/src/main/java/com/example/fitconnect/SplashActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView imageView_fitconnectLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+        imageView_fitconnectLogo = findViewById(R.id.imageView_fitconnectLogo);
+        imageView_fitconnectLogo.startAnimation(animation);
         int SPLASH_TIME_OUT = 1500;
         new Handler().postDelayed(new Runnable() {
             @Override
