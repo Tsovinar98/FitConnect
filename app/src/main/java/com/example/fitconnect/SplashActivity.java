c25cf91933cdbac93113ed19aafa7cd6592e28c2
Sovi <tgaspary@uncc.edu>
2020-11-18 17:34:50 -0500

    added fade in and fade out transitions, splash page finished

--- app/src/main/java/com/example/fitconnect/SplashActivity.java
+++ app/src/main/java/com/example/fitconnect/SplashActivity.java
@@ -2,11 +2,18 @@ package com.example.fitconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
@@ -16,15 +23,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),
-                R.anim.fade_in);
         imageView_fitconnectLogo = findViewById(R.id.imageView_fitconnectLogo);
-        imageView_fitconnectLogo.startAnimation(animation);
-        int SPLASH_TIME_OUT = 1500;
+
+        ScaleAnimation scaleUp =  new ScaleAnimation(0f, .75f, 0f, .75f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleUp.setInterpolator(new AccelerateInterpolator()); //and this
+        scaleUp.setStartOffset(0);
+        scaleUp.setFillBefore(false);
+        scaleUp.setFillAfter(true);
+        scaleUp.setDuration(2500);
+
+        AnimationSet animation = new AnimationSet(false); //change to false
+        imageView_fitconnectLogo.startAnimation(scaleUp);
+
+        int SPLASH_TIME_OUT = 2500;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 finish();
             }
         }, SPLASH_TIME_OUT);
