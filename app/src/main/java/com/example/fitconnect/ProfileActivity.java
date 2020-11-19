c65467c9be3400eff1fd027bcbb314f3a8d43092
Sovi <tgaspary@uncc.edu>
2020-11-19 01:46:27 -0500

    fragments created in viewpager on the profile activity page

--- app/src/main/java/com/example/fitconnect/ProfileActivity.java
+++ app/src/main/java/com/example/fitconnect/ProfileActivity.java
@@ -1,23 +1,50 @@
 package com.example.fitconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
     FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tabs);
 
-        currentUser = (FirebaseUser) getIntent().getExtras().get("currentUser");
-        String name = CurrentUser.getCurrentUser().getDisplayName();
-        System.out.println(name);
+        viewPager.setAdapter(createCardAdapter());
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch(position){
+                            case 0:
+                                tab.setText("PROFILE");
+                                break;
+                            case 1:
+                                tab.setText("UPCOMING EVENTS");
+                                break;
+                            case 2:
+                                tab.setText("ATTENDED EVENTS");
+                                break;
+                        }
+                    }
+                }).attach();
+    }
+
+    private ViewPagerAdapter createCardAdapter() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        return adapter;
     }
 }
