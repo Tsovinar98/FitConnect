95e0e975d92c2dd3312b460f4153893fa194d97d
Sovi <tgaspary@uncc.edu>
2020-12-14 22:20:18 -0500

    finished individual event view

---
+++ app/src/main/java/com/example/fitconnect/IndividualEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndividualEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_event);
+    }
+}
